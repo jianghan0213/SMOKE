6dd1facc7797ad106791edd6fc65db0ed8510cf6
destinyls <1491078277@qq.com>
2021-01-10 00:26:09 +0800

    weighted l1_loss

--- smoke/modeling/heads/smoke_head/loss.py
+++ smoke/modeling/heads/smoke_head/loss.py
@@ -1,10 +1,12 @@
 import torch
 from torch.nn import functional as F
+import numpy as np
 
 from smoke.modeling.smoke_coder import SMOKECoder
 from smoke.layers.focal_loss import FocalLoss
 from smoke.layers.utils import select_point_of_interest
 
+# from smoke.utils.bbox.iou_calculators.iou3d_calculator import BboxOverlaps3D
 
 class SMOKELossComputation():
     def __init__(self,
@@ -18,6 +20,16 @@ class SMOKELossComputation():
         self.reg_loss = reg_loss
         self.loss_weight = loss_weight
         self.max_objs = max_objs
+        # self.iou_calculator = BboxOverlaps3D(coordinate="camera")
+
+
+    def l1_loss(self, inputs, targets, weight, reduction="sum"):
+        diffs = torch.abs(inputs - targets) * weight
+        if reduction == "sum":
+            loss = torch.sum(diffs)
+        elif reduction == "mean":
+            loss = torch.mean(diffs)
+        return loss
 
     def prepare_targets(self, targets):
         heatmaps = torch.stack([t.get_field("hm") for t in targets])
@@ -93,10 +105,12 @@ class SMOKELossComputation():
                 targets_variables["dimensions"],
                 pred_locations
             )
-
             return dict(ori=pred_box3d_rotys,
                         dim=pred_box3d_dims,
-                        loc=pred_box3d_locs, )
+                        loc=pred_box3d_locs,
+                        pred_locs=pred_locations,
+                        pred_dims=pred_dimensions,
+                        pred_rotys=pred_rotys)
 
         elif self.reg_loss == "L1":
             pred_box_3d = self.smoke_coder.encode_box3d(
@@ -111,42 +125,62 @@ class SMOKELossComputation():
              predictions[0], predictions[1], predictions[2], predictions[3], predictions[4]
 
         predict_boxes3d = self.prepare_predictions(targets_variables, pred_regression)
-
         hm_loss = self.cls_loss(pred_heatmap, targets_heatmap) * self.loss_weight[0]
 
         # cls score
         targets_proj_points = targets_variables["proj_points"]
         batch = pred_heatmap.shape[0]
         cls_scores = select_point_of_interest(batch, targets_proj_points, pred_heatmap)
-        print("cls_scores")
-        print(cls_scores)
-        cls_scores_max = torch.max(cls_scores, -1)[0]
-        print("cls_score_max")
-        print(cls_scores_max)
-        print(cls_scores_max.shape)
+        cls_scores = torch.max(cls_scores, -1)[0].view(-1, 1)  # [240, 1]
+
+        # box iou
+        gt_locations = targets_variables["locations"].view(-1, 3)
+        gt_rotys =targets_variables["rotys"].view(-1, 1)
+        gt_dimentions = targets_variables["dimensions"].view(-1, 3)
+        gt_dimentions = gt_dimentions[:, [1, 2, 0]]  # lhw -> hwl
+        gt_boxes = torch.cat((gt_locations, gt_dimentions, gt_rotys), dim=-1)
+
+        pred_locations = predict_boxes3d["pred_locs"]
+        pred_rotys = predict_boxes3d["pred_rotys"].unsqueeze(-1)
+        pred_dimentions = predict_boxes3d["pred_dims"]
+        pred_dimentions = pred_dimentions[:, [1, 2, 0]] # lhw -> hwl
+        pred_boxes = torch.cat((pred_locations, pred_dimentions, pred_rotys), dim=-1)
+
+        # bbox_ious_3d = self.iou_calculator(gt_boxes, pred_boxes, "iou")
+        # nums_boxes = bbox_ious_3d.shape[0]
+        # bbox_ious_3d = bbox_ious_3d[range(nums_boxes), range(nums_boxes)].view(-1, 1)  # [240,]
 
         targets_regression = targets_regression.view(
             -1, targets_regression.shape[2], targets_regression.shape[3]
         )
 
         reg_mask = targets_variables["reg_mask"].flatten()
+        num_objs = torch.sum(reg_mask)
+        # reg_weight = cls_scores * (1 - bbox_ious_3d)**2
+        reg_weight = cls_scores
+        reg_weight_masked = reg_weight[reg_mask.bool()]
+        reg_weight_masked = F.softmax(reg_weight_masked, dim=0) * num_objs
+        reg_weight[reg_mask.bool()] = reg_weight_masked
+        reg_weight = reg_weight.view(-1, 1, 1)
+        reg_weight = reg_weight.expand_as(targets_regression)
         reg_mask = reg_mask.view(-1, 1, 1)
         reg_mask = reg_mask.expand_as(targets_regression)
-
+        
         if self.reg_loss == "DisL1":
-            reg_loss_ori = F.l1_loss(
+            reg_loss_ori = self.l1_loss(
                 predict_boxes3d["ori"] * reg_mask,
                 targets_regression * reg_mask,
+                reg_weight,
                 reduction="sum") / (self.loss_weight[1] * self.max_objs)
-
-            reg_loss_dim = F.l1_loss(
+            reg_loss_dim = self.l1_loss(
                 predict_boxes3d["dim"] * reg_mask,
                 targets_regression * reg_mask,
+                reg_weight,
                 reduction="sum") / (self.loss_weight[1] * self.max_objs)
-
-            reg_loss_loc = F.l1_loss(
+            reg_loss_loc = self.l1_loss(
                 predict_boxes3d["loc"] * reg_mask,
                 targets_regression * reg_mask,
+                reg_weight,
                 reduction="sum") / (self.loss_weight[1] * self.max_objs)
 
             return hm_loss, reg_loss_ori + reg_loss_dim + reg_loss_loc
